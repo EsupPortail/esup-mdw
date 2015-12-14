b0b091268ce8b639b3be52ace7769a2e6d997da3
chdubois <chdubois@70198e3a-7d99-4db3-9b1c-f0b3f7656548>
2015-12-14 13:26:55 +0000

    @Column(name="LOGIN") dans administrateurs.java
    git-svn-id: https://subversion.renater.fr/mdw-vaadin/trunk@394 70198e3a-7d99-4db3-9b1c-f0b3f7656548

--- src/main/java/fr/univlorraine/mondossierweb/entities/mdw/Administrateurs.java
+++ src/main/java/fr/univlorraine/mondossierweb/entities/mdw/Administrateurs.java
@@ -5,6 +5,7 @@ package fr.univlorraine.mondossierweb.entities.mdw;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -22,6 +23,7 @@ public class Administrateurs implements Serializable {
 	private static final long serialVersionUID = 2301719865870658196L;
 	
 	@Id
+	@Column(name="LOGIN")
 	private String login;
 
 
