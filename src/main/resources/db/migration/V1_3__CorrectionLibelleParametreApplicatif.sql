8eab7e58a2b0716ddaa3244fcd1df5be5130755c
chdubois <chdubois@70198e3a-7d99-4db3-9b1c-f0b3f7656548>
2016-03-03 08:50:55 +0000

    correction du libellé de 2 paramètres applicatifs
    git-svn-id: https://subversion.renater.fr/mdw-vaadin/trunk@451 70198e3a-7d99-4db3-9b1c-f0b3f7656548

---
+++ src/main/resources/db/migration/V1_3__CorrectionLibelleParametreApplicatif.sql
@@ -0,0 +1,18 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Version du serveur: 5.5.34-log
+-- Version de PHP: 5.3.3
+
+
+-- ESUP-Portail MONDOSSIERWEB - Copyright (c) 2016 ESUP-Portail consortium
+-- Base de donnees: `mdw`
+--
+
+-- --------------------------------------------------------
+
+update preferences_application set PREF_DESC='Liste des codes CGE pour lesquels la generation de certificat est desactivee.' where PREF_ID='certScolCGEDesactive';
+
+update preferences_application set PREF_DESC='Liste des codes de types de diplomes pour lesquels la generation de certificat est desactivee.' where PREF_ID='certScolTypDiplomeDesactive';
+
