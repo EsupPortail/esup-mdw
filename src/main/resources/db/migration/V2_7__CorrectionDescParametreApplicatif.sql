bca136169a54b1ee50b5b069570dfc72c2cc65ef
Charlie <charlie.dubois@univ-lorraine.fr>
2020-03-24 15:30:06 +0100

    correction du libellé descriptif du paramètre applicatif 'affInfosContactEnseignants'

---
+++ src/main/resources/db/migration/V2_7__CorrectionDescParametreApplicatif.sql
@@ -0,0 +1,15 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Version du serveur: 5.5.34-log
+-- Version de PHP: 5.3.3
+
+
+-- ESUP-Portail MONDOSSIERWEB - Copyright (c) 2016 ESUP-Portail consortium
+-- Base de donnees: `mdw`
+--
+
+-- --------------------------------------------------------
+
+UPDATE `PREFERENCES_APPLICATION` SET `PREF_DESC`='Afficher les infos de contact (tel portable) aux enseignants : true pour activer, false sinon' WHERE `PREF_ID`='affInfosContactEnseignants';
