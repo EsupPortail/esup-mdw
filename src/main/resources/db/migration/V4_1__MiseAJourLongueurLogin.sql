6d5fefae243b11188855b7535c655def0391eeea
Charlie <charlie.dubois@univ-lorraine.fr>
2021-10-18 16:17:57 +0200

    maj longueur login dans la base

---
+++ src/main/resources/db/migration/V4_1__MiseAJourLongueurLogin.sql
@@ -0,0 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Version du serveur: 5.5.34-log
+-- Version de PHP: 5.3.3
+
+
+-- ESUP-Portail MONDOSSIERWEB - Copyright (c) 2016 ESUP-Portail consortium
+-- Base de donnees: `mdw`
+--
+
+-- --------------------------------------------------------
+
+ALTER TABLE FAVORIS MODIFY LOGIN VARCHAR(100);
+
+ALTER TABLE PREFERENCES_UTILISATEUR MODIFY LOGIN VARCHAR(100);
+
+
+
+
+
+
+
