3791227d50fa03b03bfffc741ac8256e121f54bf
Charlie <charlie.dubois@univ-lorraine.fr>
2022-06-08 10:12:03 +0200

    Indication que les paramètres certificatScolaritePdfPositionSignature et quittancePdfPositionSignature sont maintenant ignorés

---
+++ src/main/resources/db/migration/V4_2__DepreciationPositionSignature.sql
@@ -0,0 +1,20 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Version du serveur: 5.5.34-log
+-- Version de PHP: 5.3.3
+
+
+-- ESUP-Portail MONDOSSIERWEB - Copyright (c) 2016 ESUP-Portail consortium
+-- Base de donnees: `mdw`
+--
+
+-- --------------------------------------------------------
+
+UPDATE preferences_application set PREF_DESC = '[INUTILISE] Paramètre ignoré en version 1.6.8' where PREF_ID = 'certificatScolaritePdfPositionSignature';
+
+
+UPDATE preferences_application set PREF_DESC = '[INUTILISE] Paramètre ignoré en version 1.6.8' where PREF_ID = 'quittancePdfPositionSignature';
+
+
