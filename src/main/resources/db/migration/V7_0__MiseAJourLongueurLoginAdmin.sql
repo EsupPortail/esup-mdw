41d8f797af25ad1b52baa3131f4d98d1ba8e9cf3
Charlie <charlie.dubois@univ-lorraine.fr>
2024-05-13 09:38:18 +0200

    Colonne "login" de la table "administrateurs" passe sur 100 caractères

---
+++ src/main/resources/db/migration/V7_0__MiseAJourLongueurLoginAdmin.sql
@@ -0,0 +1,21 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Version du serveur: 5.5.34-log
+-- Version de PHP: 5.3.3
+
+
+-- ESUP-Portail MONDOSSIERWEB - Copyright (c) 2016 ESUP-Portail consortium
+-- Base de donnees: `mdw`
+--
+
+-- --------------------------------------------------------
+
+ALTER TABLE ADMINISTRATEURS MODIFY LOGIN VARCHAR(100);
+
+
+
+
+
+
