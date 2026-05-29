9c130e7e10f1302739e31cfadf89ffdee088c709
Charlie <charlie.dubois@univ-lorraine.fr>
2026-05-29 15:57:04 +0200

    Correction bug /link/accesDossierEtudiant

--- src/main/java/fr/univlorraine/mondossierweb/controllers/rest/DeepLinking.java
+++ src/main/java/fr/univlorraine/mondossierweb/controllers/rest/DeepLinking.java
@@ -18,16 +18,14 @@
  */
 package fr.univlorraine.mondossierweb.controllers.rest;
 
+import fr.univlorraine.mondossierweb.utils.Utils;
+import fr.univlorraine.mondossierweb.views.AdminView;
 import jakarta.annotation.Resource;
-
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import fr.univlorraine.mondossierweb.utils.Utils;
-import fr.univlorraine.mondossierweb.views.AdminView;
 
 
 /**
@@ -46,7 +44,7 @@ public class DeepLinking {
 	 * redirige vers la bonne url (apres être passé par le CAS)
 	 */
 	@RequestMapping(value= "/" + Utils.FRAGMENT_ACCES_DOSSIER_ETUDIANT + "/{codEtu}", method=RequestMethod.GET)
-	public String accesDossier(@PathVariable String codEtu) {
+	public String accesDossier(@PathVariable("codEtu") String codEtu) {
 
 		String path = environment.getRequiredProperty("app.url") + VAADIN_FRAGMENT_PREFIX + Utils.FRAGMENT_ACCES_DOSSIER_ETUDIANT + "/" + codEtu;
 		
